f276425e01cd01d33519fc0798f060e02535242e
mhhaekal <mhaekal55@gmail.com>
2023-09-21 15:55:38 +0700

    Fix

--- client/src/Component/Button.jsx
+++ client/src/Component/Button.jsx
@@ -1,7 +1,7 @@
 const Button = (props) => {
     return (
         <div className="flex relative">
-            <button onClick={props.click} className={props.style}>
+            <button onClick={props.onClick} className={props.style}>
                 {props.text}
             </button>
             <div className="absolute -right-2">{props.item}</div>
